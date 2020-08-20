26e573ec78c13d174e3285671c816b13683e8324
sifat007 <sifat27.ht@gmail.com>
2020-08-20 17:30:59 +0600

    make file folder for room

---
+++ app/src/main/java/com/example/roominandroid/Room/DAO.java
@@ -0,0 +1,8 @@
+package com.example.roominandroid.Room;
+import androidx.room.Dao;
+/**That class call DAO (Data access object), here we make functionality for insert,update,delete (query) ***/
+
+@Dao
+public interface DAO {
+
+}
---
+++ app/src/main/java/com/example/roominandroid/Room/MyDatabase.java
@@ -0,0 +1,16 @@
+package com.example.roominandroid.Room;
+
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+/**That class call DAtabase, here we make datase here all main work will perform like inser delete update all work perform here ***/
+
+
+
+@Database(entities = {Student.class},version = 1,exportSchema = false)
+public abstract class MyDatabase extends RoomDatabase {
+
+    public abstract DAO dao();
+
+}
